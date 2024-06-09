cc4ec51a7e3d3e552091010a68c9cd9d1d1a09f7
ArsalanSiddiqui195 <arsalan.siddiqui195@gmail.com>
2024-06-09 12:07:22 +0500

    Example of Variables using in Dart

---
+++ Variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  //var keyword includes all data types
+  var name = 'John';
+  print(name);
+  String address = '112-F,florida';
+  num age = 20; //includes both integer and floating point
+  double height = 5.6;
+  bool married = true;
+  print(
+      'hello, the address is $address. My Height is $height and age is $age. And I am not $married');
+}
