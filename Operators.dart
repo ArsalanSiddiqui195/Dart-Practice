6f2307fd31463eedbc3b2540ee6e3794db0fe5c2
ArsalanSiddiqui195 <arsalan.siddiqui195@gmail.com>
2024-06-09 16:13:10 +0500

    Different Operators

---
+++ Operators.dart
@@ -0,0 +1,32 @@
+void main() {
+  //airthemetic operations
+  int a = 30;
+  int b = 10;
+  int sum = a + b;
+  int dif = a - b;
+  int mul = a * b;
+  double div = a / b;
+  int mod = a % b;
+  int integer = a ~/ b;
+  int uniaryMinus = -a;
+  print(sum);
+  print(dif);
+  print(mul);
+  print(div);
+  print(mod);
+  print(integer);
+  print(uniaryMinus);
+
+//Increment/DecrementOperators
+  int c = 20;
+  int d = 30;
+  c = ++c;
+  print(c);
+  c = --c;
+  print(c);
+  d = d++;
+  print(d);
+  d = d--;
+  print(d);
+}
+//Assignment,Relational,Logical Operators
