65b53f4133841f8a856a0e9aff6dc761e31827eb
ArsalanSiddiqui195 <arsalan.siddiqui195@gmail.com>
2024-06-09 16:48:25 +0500

    List and its methods

---
+++ Lists.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<int> integers = [1, 2, 3, 4];
+  print(integers[3]);
+  List<String> names = ['ali', 'ahmemd', 'razaq'];
+  print(names[0]);
+
+//Methods of Lists
+  int a = names.length;
+  print(a);
+  var firstName = names.first;
+  print(firstName);
+  var abc = names.reversed;
+  print(abc);
+}
