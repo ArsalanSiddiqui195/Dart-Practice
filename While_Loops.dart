1c641c369dac540ad841fee04c59fd9c7dd90cfc
ArsalanSiddiqui195 <arsalan.siddiqui195@gmail.com>
2024-06-11 18:21:40 +0500

    While Loop

---
+++ While_Loops.dart
@@ -0,0 +1,17 @@
+//Simple While Loop
+import 'dart:io';
+
+void main() {
+  // int a = 0;
+  // while (a < 5) {
+  //   print(a);
+  //   a++;
+  // }
+  // int? num1 = int.parse(stdin.readLineSync()!);
+  // String? email = stdin.readLineSync();
+  // print("My email address is: $email");
+  // print("My email address is: $num1");
+  var password = stdin.readLineSync();
+  bool isLogin = false;
+  while (isLogin == false) {}
+}
