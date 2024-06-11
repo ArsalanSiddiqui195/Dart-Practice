79add57b252a9d3573f47d9148dbc550eaae61fc
ArsalanSiddiqui195 <arsalan.siddiqui195@gmail.com>
2024-06-11 22:42:04 +0500

    Functions

---
+++ functions.dart
@@ -0,0 +1,32 @@
+// void main() {
+//   studentMarksheet();
+//   var abc = studentMarksheet();
+//   print(abc);
+// }
+
+// studentMarksheet() {
+//   String name = 'bilal';
+//   num subject1 = 76;
+//   num subject2 = 62;
+//   num subject3 = 90;
+//   num obtainMarks = subject1 + subject2 + subject3;
+//   num percentage = (obtainMarks / 300) * 100;
+//   if (percentage >= 50) {
+//     print("$name is passed");
+//     return "pass";
+//    } else {
+//     print("$name is failed");
+//     return "fail";
+//   }
+// }
+
+void main() {
+  var a = sumOfTwoNumbers();
+  print(a);
+}
+
+num sumOfTwoNumbers() {
+  num num1 = 2;
+  num num2 = 4;
+  return num1 + num2;
+}
